4ea605454faf6bb367e05ff6863b6309f2553baf
Rei-Yamaguchi <imaime03@gmail.com>
2021-11-20 00:20:03 +0000

    finish 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :todos
-  resources :goals
+  # goalsの配下でdo以下のブロックが回るイメージ
+    resources :goals do
+      resources :todos do
+        member do
+        # /goals/:goal_id/todos/:id/sort というURLに対して
+        # todos#sort というアクションを割り当てる
+        get "sort"
+      end
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
